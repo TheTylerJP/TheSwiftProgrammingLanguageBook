2d2c74e20e64b7fd3a1a0aaf87b44886548cabf5
Tyler Phillips <thetylerjp@icloud.com>
2019-01-24 12:36:28 -0500

    The Basics Initial Commit

---
+++ The Basics/SwiftProgrammingLanguage-TheBasics.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+//Constant
+let maximumNumberOfLoginAttempts = 10
+
+//Variable
+var currentLoginAttempt = 0
+
+//Multiple Variables in one line as Floats
+var x = 0.0, y = 10.0, a = 23.0
+
+//Type Annotations
+var welcomeMessage: String
+
+
